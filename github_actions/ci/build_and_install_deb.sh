97da2bab223295909be47562513db0c8b7f414da
epi-mai <m.alali@epiconcept.fr>
2024-04-09 16:46:20 +0200

    test

--- github_actions/ci/build_and_install_deb.sh
+++ github_actions/ci/build_and_install_deb.sh
@@ -14,7 +14,18 @@ _APP_SOURCE_CODE=$2
 _APPV=$(jq -r .PACKAGING.INF_VERSION $2/configs/APPINFOS)
 _PKG_LOWER_NAME=$(jq -r .PACKAGING.INF_PACKAGE $2/configs/APPINFOS | tr '[:upper:]' '[:lower:]')
 
-exit 0
+
 [[ -z ${_APPNAME} || -z ${_APPV} ]] &&
   echo "[ERROR] ${0##*/}: missing argument (voo4version appname appversion)" &&
   exit 1
+  
+  
+  
+echo "Start building .deb"
+
+# Expected branch name: release-2.24 => 2.24
+_VOO4V=$(echo $GITHUB_REF | sed 's/.*- *\([0-9]\{1,2\}.[0-9]\{1,3\}\).*/\1/')
+# Add suffix "-alpha" to version number, used by staging branch, ex: 2.24-alpha
+if [[ $GITHUB_REF =~ ^staging-.*$ ]]; then
+    _VOO4V=$_VOO4V"-alpha"
+fi
